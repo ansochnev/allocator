81070f30294750d7a1b5944d2c7a783d3db9a2c4
Anton Sochnev <an.sochnev@yandex.ru>
2019-02-10 16:05:44 +0300

    add test for allocator

---
+++ alloc_test.cpp
@@ -0,0 +1,37 @@
+#include <cassert>
+#include "alloc.h"
+
+int main() {
+    Allocator<int, 3> alloc;
+    
+    int* p = alloc.allocate(3);
+
+    alloc.construct<int, int>(p, 28);
+    alloc.construct<int, int>(++p, 42);
+    alloc.construct<int, int>(++p, 13);
+    
+    assert(*p-- == 13);
+    assert(*p-- == 42);
+    assert(*p == 28);
+
+    p += 2;
+
+    alloc.destroy(p);
+    alloc.deallocate(p, 1);
+
+    alloc.allocate(1);
+    alloc.construct(p, 35);
+
+    assert(*p == 35);
+
+    alloc.destroy(p--);
+    alloc.destroy(p--);
+    alloc.destroy(p);
+
+    alloc.deallocate(p, 3);
+
+    p = alloc.allocate(1);
+    alloc.construct<int, int>(p, 100);
+
+    assert(*p == 100);
+}
